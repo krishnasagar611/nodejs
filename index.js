7eaaa3f2e6fa8b9c82ee84b8c3459dd710440883
Sagar Kumar Rath <101728627+krishnasagar611@users.noreply.github.com>
2023-09-19 21:01:49 +0530

    error fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = 5000; // You can choose any available port
-const todoService = require('./service/service.todo') 
+const todoService = require('./service/service.todo')
 const cors = require('cors');
 const app = express();
 
@@ -11,33 +11,33 @@ app.use(cors());
 //For the starting url
 app.get('/', (req, res) => {
     res.send('This is a backend server there will no views,So hit the request from the ui or postman.');
-  });
+});
 
 //To get all todo hit this url
 app.get('/todos', (req, res) => {
-    todoService.getAllTodo(req,res);
+    todoService.getAllTodo(req, res);
 });
 
 //To get the todo by its id
 app.get('/todo', (req, res) => {
-    todoService.getTodoByID(req,res);
+    todoService.getTodoByID(req, res);
 });
 
 //To create a new TODO
 app.post('/todo', (req, res) => {
-    todoService.createTodo(req,res);
+    todoService.createTodo(req, res);
 });
 
 
 //To update a TODO
 app.put('/todo', (req, res) => {
-    todoService.updateTodo(req,res);
+    todoService.updateTodo(req, res);
 });
 
 
 //To delete the TODO
 app.delete('/todo', (req, res) => {
-    todoService.deleteTodo(req,res);
+    todoService.deleteTodo(req, res);
 });
 
 
